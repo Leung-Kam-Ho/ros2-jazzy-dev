c11f878152631bf5a12a1a2c98399ffeec64bb25
Leung-Kam-Ho <kamho.leung@polyu.edu.hk>
2026-06-15 03:03:39 +0800

    Fix zenoh download URLs in Linux install script

--- native_linux/1_install_zenoh.sh
+++ native_linux/1_install_zenoh.sh
@@ -5,10 +5,10 @@ ARCH=$(uname -m)
 
 if [ "$ARCH" = "x86_64" ]; then
     echo "Downloading Zenoh DDS Bridge for x86_64 (amd64)..."
-    curl -L -o zenoh-bridge-dds.zip https://github.com/eclipse-zenoh/zenoh-plugin-dds/releases/download/1.0.2/zenoh-bridge-dds-1.0.2-x86_64-unknown-linux-gnu.zip
+    curl -L -o zenoh-bridge-dds.zip https://github.com/eclipse-zenoh/zenoh-plugin-dds/releases/download/1.0.2/zenoh-plugin-dds-1.0.2-x86_64-unknown-linux-gnu-standalone.zip
 elif [ "$ARCH" = "aarch64" ]; then
     echo "Downloading Zenoh DDS Bridge for aarch64 (arm64)..."
-    curl -L -o zenoh-bridge-dds.zip https://github.com/eclipse-zenoh/zenoh-plugin-dds/releases/download/1.0.2/zenoh-bridge-dds-1.0.2-aarch64-unknown-linux-gnu.zip
+    curl -L -o zenoh-bridge-dds.zip https://github.com/eclipse-zenoh/zenoh-plugin-dds/releases/download/1.0.2/zenoh-plugin-dds-1.0.2-aarch64-unknown-linux-gnu-standalone.zip
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
