e7d375bee3c2082d4b49911e56c74531a2ee1f36
Leung-Kam-Ho <kamho.leung@polyu.edu.hk>
2026-06-17 23:00:14 +0800

    Fix DISPLAY environment variable assignment in run_r2_jazzy.sh

--- scripts/run_r2_jazzy.sh
+++ scripts/run_r2_jazzy.sh
@@ -10,8 +10,8 @@ DISPLAY_VAL="${DISPLAY:-host.docker.internal:0}"
 docker run -it --rm \
     --name "$NAME" \
     --network host \
-    -e DISPLAY="docker.for.mac.host.internal:0" \
     -e ROS_DOMAIN_ID="$DOMAIN_ID" \
+    -e DISPLAY="docker.for.mac.host.internal:0" \
     -e LIBGL_ALWAYS_INDIRECT=1 \
     -e QT_XCB_GL_INTEGRATION=none \
     r2_jazzy
